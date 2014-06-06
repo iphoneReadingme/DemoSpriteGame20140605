25305566b0597791fccb3f6b860aab95c96b8cee
iphoneReadingme <readingme@126.com>
2014-06-06 14:43:11 +0800

    添加sprite节点子类

---
+++ DemoSpriteGame20140605/DemoClass20140606/DemoSource/ObjectiveCCode/DGSpaceshipSprite.mm
@@ -0,0 +1,14 @@
+//
+//  DemoSpriteGameViewController.m
+//  DemoSpriteGame
+//
+//  Created by yangfs on 14-6-6.
+//  Copyright (c) 2014年 ucweb. All rights reserved.
+//
+
+
+#import "DGSpaceshipSprite.h"
+
+@implementation DGSpaceshipSprite
+
+@end
--- DemoSpriteGame20140605/DemoSpriteGame/DemoSpriteGameScene.mm
+++ DemoSpriteGame20140605/DemoSpriteGame/DemoSpriteGameScene.mm
@@ -24,30 +24,65 @@
                                        CGRectGetMidY(self.frame));
         
         [self addChild:myLabel];
+		
+		///< 添加节点
+		CGPoint pt = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+		[self addSpriteObject:pt];
     }
     return self;
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
+	
+	for (UITouch *touch in touches)
+	{
+		//CGPoint location = [touch locationInNode:self];
+		
+		//[self addSpriteObject:location];
+		//NSLog(@"===location=%@", NSStringFromCGPoint(location));
     }
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	for (UITouch *touch in touches)
+	{
+		CGPoint location = [touch locationInNode:self];
+		
+		//[self addSpriteObject:location];
+		NSLog(@"===location=%@", NSStringFromCGPoint(location));
+		int nCount = [touch tapCount];
+		if (nCount == 2)
+		{
+			///< 双击打印测试信息
+			//self.touchOne()
+		}
+		else if (nCount == 1)
+		{
+			;
+		}
+	}
 }
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
 }
+
+
+///< 添加一个sprite对象(渲染节点)
+- (void)addSpriteObject:(CGPoint)location
+{
+	SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+	
+	sprite.position = location;
+	
+	SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+	
+	[sprite runAction:[SKAction repeatActionForever:action]];
+	
+	[self addChild:sprite];
+}
+
 
 @end
